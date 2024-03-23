c69c92341d40ef2a601a727217e248e3ad2e5225
hasanFC724 <164681048+hasanFC724@users.noreply.github.com>
2024-03-23 20:41:59 +0000

    updated app.py to use flaskwtf

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, redirect, url_for
+from forms import AssessmentForm  # Import the form class
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'  # Set a secret key for CSRF protectio
 
 @app.route('/')
 def index():
@@ -14,9 +15,13 @@ def home():
 def about():
     return render_template('information.html')
 
-@app.route('/form')
+@app.route('/form', methods=['GET', 'POST'])
 def contact():
-    return render_template('form.html')
+    form = AssessmentForm()
+    if form.validate_on_submit():
+        # Process the form data, save it, send it, etc.
+        return redirect(url_for('home'))  # Redirect to a different page after submit
+    return render_template('form.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
